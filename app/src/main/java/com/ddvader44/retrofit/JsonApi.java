77a31c5332bc58f9f6471d1429c54a81dfa4a131
Dishit <40226856+ddvader44@users.noreply.github.com>
2020-07-18 23:12:55 +0530

    update/delete

--- app/src/main/java/com/ddvader44/retrofit/JsonApi.java
+++ app/src/main/java/com/ddvader44/retrofit/JsonApi.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -32,4 +35,13 @@ public interface JsonApi {
             @Field("title") String title,
             @Field("body") String text
     );
+
+    @PUT("posts/{id}")
+    Call<Post> putPost(@Path("id") int id, @Body Post post);
+
+    @PATCH("posts/{id}")
+    Call<Post> patchPost(@Path("id") int id, @Body Post post);
+
+    @DELETE("posts/{id}")
+    Call<Void> deletePost(@Path("id") int id);
 }
--- app/src/main/java/com/ddvader44/retrofit/MainActivity.java
+++ app/src/main/java/com/ddvader44/retrofit/MainActivity.java
@@ -34,7 +34,58 @@ public class MainActivity extends AppCompatActivity {
 
        // getPosts();
         //getComments();
-        createPosts();
+        //createPosts();
+        //updatePost();
+        deletePost();
+    }
+
+    private void deletePost() {
+        Call<Void> call = jsonApi.deletePost(5);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                textViewResult.setText("Code:- "+response.code());
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void updatePost() {
+        Post post = new Post(12,null,"New");
+        Call<Post> call = jsonApi.putPost(3,post);
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                if(!response.isSuccessful())
+                {
+                    textViewResult.setText("Code: "+ response.code());
+                    return;
+                }
+
+                Post postResponse = response.body();
+
+
+                String content="";
+                content += "Code: "+response.code()+"\n";
+                content += "ID: "+postResponse.getId()+"\n";
+                content += "User ID: "+postResponse.getUserId()+"\n";
+                content += "Title: "+postResponse.getTitle()+"\n";
+                content += "Text: "+postResponse.getText()+"\n\n";
+
+                textViewResult.setText(content);
+
+
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
     }
 
     private void createPosts() {
